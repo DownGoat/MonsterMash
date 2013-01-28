4e263f0cd731085995e224400e56c7fbe14d9aff
sis13 <sis13@144.124.57.194>
2013-01-28 17:26:35 +0000

    Changed Monster, started on the get monster api.

--- G12/src/MonsterMash/ServerCom/JSONManager.java
+++ G12/src/MonsterMash/ServerCom/JSONManager.java
@@ -4,6 +4,7 @@
  */
 package ServerCom;
 
+import data.Monster;
 import data.Player;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,15 +24,34 @@ public class JSONManager {
      */
     public static JSONObject jsonPlayer(Player player) {
         JSONObject jObj = new JSONObject();
-          
+
         try {
             jObj.put("money", player.getMoney());
-            jObj.put("userID", String.valueOf(player.getId()));
+            jObj.put("userID", player.getEmail());
             jObj.put("name", player.getEmail());
         } catch (JSONException ex) {
             Logger.getLogger(JSONManager.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
+        return jObj;
+    }
+
+    public static JSONObject jsonMonster(Monster monster) {
+        JSONObject jObj = new JSONObject();
+
+        try {
+            jObj.put("monsterID", String.valueOf(monster.getId()));
+            jObj.put("userID", String.valueOf(monster.getId()));
+            jObj.put("baseStrength", monster.getGenetic_strength());
+            jObj.put("defence", monster.getArmor());
+            jObj.put("dob", monster.getDob().getTime());
+            jObj.put("dod", monster.getDod().getTime());
+            jObj.put("forSale", monster.getForSale());
+            jObj.put("forBreed", monster.getForBreed());
+        } catch (JSONException ex) {
+            Logger.getLogger(JSONManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
         return jObj;
     }
 }
