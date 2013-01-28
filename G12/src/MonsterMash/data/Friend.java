40650fcf060a55515b38456f06a63d7a491b2853
Llion Vaughan <lwv@aber.ac.uk>
2013-01-28 14:44:51 +0000

    JUnit Testing for Friend.

--- G12/src/MonsterMash/data/Friend.java
+++ G12/src/MonsterMash/data/Friend.java
@@ -5,17 +5,36 @@ package data;
  * @author sjk4
  */
 public class Friend {
+
     private int id;
     private String email;
     private boolean friendshipConfirmed;
-    
-    public Friend(int id, String email, String confirmed){
+
+
+
+    public Friend(int id, String email, String confirmed) {
         this.id = id;
         this.email = email;
-        if(confirmed.equals("Y")){
+        if (confirmed.equals("Y")) {
             friendshipConfirmed = true;
-        }else{
+        } else {
             friendshipConfirmed = false;
         }
+    }
+
+    public int getId(Friend f) {
+        return f.id;
+    }
+
+    public void setFriendConfirmed(String confirmed) {
+        if (confirmed.equals("Y")) {
+            friendshipConfirmed = true;
+        } else {
+            friendshipConfirmed = false;
+        }
+    }
+    
+        public boolean isFriendshipConfirmed(Friend f) {
+        return f.friendshipConfirmed;
     }
 }
