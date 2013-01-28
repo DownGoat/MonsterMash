550d0648f2843a2c0dd0d961cd47f989b9ef3e0d
sjk4 <stanislaw.klimaszewski@wp.pl>
2013-01-28 14:46:42 +0000

    Store friendship id, friend id and server id for each friendship.

--- G12/src/MonsterMash/data/Friend.java
+++ G12/src/MonsterMash/data/Friend.java
@@ -5,12 +5,15 @@ package data;
  * @author sjk4
  */
 public class Friend {
-    private int id;
+    private int friendshipID;
+    private int friendID, serverID;
     private String email;
     private boolean friendshipConfirmed;
     
-    public Friend(int id, String email, String confirmed){
-        this.id = id;
+    public Friend(int friendID, int serverID, String email, String confirmed){
+        this.friendshipID = 0;
+        this.friendID = friendID;
+        this.serverID = serverID;
         this.email = email;
         if(confirmed.equals("Y")){
             friendshipConfirmed = true;
@@ -19,12 +22,28 @@ public class Friend {
         }
     }
 
-    public int getId() {
-        return id;
+    public int getFriendshipID() {
+        return friendshipID;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setFriendshipID(int friendshipID) {
+        this.friendshipID = friendshipID;
+    }
+
+    public int getFriendID() {
+        return friendID;
+    }
+
+    public void setFriendID(int friendID) {
+        this.friendID = friendID;
+    }
+
+    public int getServerID() {
+        return serverID;
+    }
+
+    public void setServerID(int serverID) {
+        this.serverID = serverID;
     }
 
     public String getEmail() {
