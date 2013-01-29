1f4cffc51e6e68dada8f0fa63f6dc3d149f1f37b
sjk4 <stanislaw.klimaszewski@wp.pl>
2013-01-29 10:41:30 +0000

    Fixed highscores.

--- G12/src/MonsterMash/HighscoresPage.java
+++ G12/src/MonsterMash/HighscoresPage.java
@@ -6,7 +6,6 @@
 import data.Player;
 import database.PersistenceManager;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
