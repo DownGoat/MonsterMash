d3181a95483ac07c0d6703727853b35d896003cc
Llion Vaughan <lwv@aber.ac.uk>
2013-01-29 11:14:32 +0000

    Updated JUnit Tests for  monster. (For Now)

--- Test Packages/data/MonsterTest.java
+++ Test Packages/data/MonsterTest.java
@@ -43,7 +43,7 @@ public class MonsterTest {
     @Test
     public void testGetId() {
         System.out.println("getId");
-        Monster monster = new Monster("Bob");
+        Monster monster = new Monster("Bob",1);
         assertEquals("Expected id = 0", 0,monster.getId());
     }
 
@@ -53,7 +53,7 @@ public class MonsterTest {
     @Test
     public void testSetId() {
         System.out.println("setId");
-        Monster monster = new Monster("Bob");
+        Monster monster = new Monster("Bob",1);
         monster.setId(23);
         assertEquals("Expected id = 23", 23,monster.getId());
     }
@@ -64,7 +64,7 @@ public class MonsterTest {
     @Test
     public void testGetName() {
         System.out.println("getName");
-         Monster monster = new Monster("Bob");
+         Monster monster = new Monster("Bob",1);
          assertEquals("Expected name 'Bob'","Bob",monster.getName());
          
     
@@ -76,7 +76,7 @@ public class MonsterTest {
     @Test
     public void testSetName() {
         System.out.println("setName");
-        Monster monster = new Monster("Bob");
+        Monster monster = new Monster("Bob",1);
         monster.setName("Bill");
         assertEquals("Expected name 'Bill'","Bill", monster.getName());
         
@@ -88,115 +88,95 @@ public class MonsterTest {
     @Test
     public void testGetDob() {
         System.out.println("getDob");
-        Monster monster = new Monster("Bob");
+        Monster monster = new Monster("Bob",1);
         Date now = new Date();
         assertEquals("That now = the monsters dob",now,monster.getDob());
     }
 
 
     /**
-     * Test of getGenetic_strength method, of class Monster.
+     * Test of getBaseStrength method, of class Monster.
      */
     @Test
-    public void testGetGenetic_strength() {
-        System.out.println("getGenetic_strength");
-        Monster monster = new Monster("Bob");
-        assertTrue(0<monster.getStrength() && monster.getStrength()<1);
+    public void testGetBaseStrength() {
+        System.out.println("getBaseStrength");
+        Monster monster = new Monster("Bob",1);
+        assertTrue(0<monster.getBaseStrength() && monster.getBaseStrength()<1);
     }
 
-    /**
-     * Test of getSpeed method, of class Monster.
+       /**
+     * Test of getCurrentStrength method, of class Monster.
      */
     @Test
-    public void testGetSpeed() {
-        System.out.println("getSpeed");
-        Monster monster = new Monster("Bob");
-        assertTrue(0<monster.getSpeed() && monster.getSpeed()<1);
-    }
-
-    /**
-     * Test of getAccuracy method, of class Monster.
-     */
-    @Test
-    public void testGetAccuracy() {
-        System.out.println("getAccuracy");
-        Monster monster = new Monster("Bob");
-        assertTrue(0<monster.getAccuracy() && monster.getAccuracy()<1);
+    public void testCurrentStrength() {
+        System.out.println("getCurrentStrength");
+        Monster monster = new Monster("Bob",1);
+        assertTrue(0<monster.getCurrentStrength() && monster.getCurrentStrength()<1);
     }
 
 
+
     /**
-     * Test of getEndurance method, of class Monster.
+     * Test of getBaseDefence method, of class Monster.
      */
     @Test
-    public void testGetEndurance() {
-        System.out.println("getEndurance");
-        Monster monster = new Monster("Bob");
-        assertTrue(0<monster.getEndurance() && monster.getEndurance()<1);
+    public void testGetBaseDefence() {
+        System.out.println("BaseDefence");
+        Monster monster = new Monster("Bob",1);
+        assertTrue(0<monster.getBaseDefence() && monster.getBaseDefence()<1);
+    }
+
+
+    /**
+     * Test of getCurrentDefence method, of class Monster.
+     */
+    @Test
+    public void testCurrentDefence() {
+        System.out.println("CurrentDefence");
+        Monster monster = new Monster("Bob",1);
+        assertTrue(0<monster.getCurrentDefence() && monster.getCurrentDefence()<1);
     }
 
     /**
-     * Test of getArmor method, of class Monster.
+     * Test of getBaseHealth method, of class Monster.
      */
     @Test
-    public void testGetArmor() {
-        System.out.println("getArmor");
-        Monster monster = new Monster("Bob");
-        assertTrue(0<monster.getArmor() && monster.getArmor()<1);
+    public void testGetBaseHealth() {
+        System.out.println("getBaseHealth");
+        Monster monster = new Monster("Bob",1);
+        assertTrue(0<monster.getBaseHealth() && monster.getBaseHealth()<1);
     }
 
     /**
-     * Test of getDodge method, of class Monster.
+     * Test of getCurrentHealth method, of class Monster.
      */
     @Test
-    public void testGetDodge() {
-        System.out.println("getDodge");
-        Monster monster = new Monster("Bob");
-        assertTrue(0<monster.getDodge() && monster.getDodge()<1);
+    public void testGetCurrentHealth() {
+        System.out.println("getCurrentHealth");
+        Monster monster = new Monster("Bob",1);
+        assertTrue(0<monster.getCurrentHealth() && monster.getCurrentHealth()<1);
     }
 
     /**
      * Test of getAge_rate method, of class Monster.
-     */
+    
     @Test
     public void testGetAge_rate() {
         System.out.println("getAge_rate");
         Monster monster = new Monster("Bob");
         assertTrue(0<monster.getAge_rate() && monster.getAge_rate()<1);
     }
-
+ */
     /**
      * Test of getFertility method, of class Monster.
      */
     @Test
     public void testGetFertility() {
         System.out.println("getFertility");
-        Monster monster = new Monster("Bob");
+        Monster monster = new Monster("Bob",1);
         assertTrue(0<monster.getFertility() && monster.getFertility()<1);
     }
 
-
-    /**
-     * Test of getHealth method, of class Monster.
-     */
-    @Test
-    public void testGetHealth() {
-        System.out.println("getHealth");
-        Monster monster = new Monster("Bob");
-        assertEquals("Health should = 100",100, monster.getHealth(),0);
-    }
-
-
-
-    /**
-     * Test of getStrength method, of class Monster.
-     */
-    @Test
-    public void testGetStrength() {
-        System.out.println("getStrength");
-        Monster monster = new Monster("Bob");
-        assertTrue(0<monster.getStrength() && monster.getStrength()<1);
-    }
 
 
 }
