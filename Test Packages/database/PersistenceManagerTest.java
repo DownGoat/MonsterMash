b22bc82c19f8ad184f9eb23f4c091715aab6b37d
Llion Vaughan <lwv@aber.ac.uk>
2013-01-30 10:12:28 +0000

    Added new PersistenceManagerTest

--- Test Packages/database/PersistenceManagerTest.java
+++ Test Packages/database/PersistenceManagerTest.java
@@ -4,6 +4,7 @@
  */
 package database;
 
+import data.Monster;
 import data.Player;
 import java.util.ArrayList;
 import org.junit.After;
@@ -44,47 +45,50 @@ public class PersistenceManagerTest {
     @Test
     public void testAccountExists() {
         System.out.println("accountExists");
-     PersistenceManager pm = new PersistenceManager();
-     Player p = new Player("jau1@aber.ac.uk", "password", 5, "Bill");
-     pm.addNewPlayer(p);
-     assertTrue(pm.accountExists("jau1@aber.ac.uk"));
-    }
-
-    /**
-     * Test of addNewPlayer method, of class PersistenceManager.
-     */
-    @Test
-    public void testAddNewPlayer() {
-        System.out.println("addNewPlayer");
-        Player p = null;
+        String userID = "";
         PersistenceManager instance = new PersistenceManager();
-        instance.addNewPlayer(p);
+        boolean expResult = false;
+        boolean result = instance.accountExists(userID);
+        assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
 
     /**
-     * Test of addNewNotifications method, of class PersistenceManager.
+     * Test of storePlayer method, of class PersistenceManager.
      */
     @Test
-    public void testAddNewNotifications() {
-        System.out.println("addNewNotifications");
+    public void testStorePlayer() {
+        System.out.println("storePlayer");
         Player p = null;
         PersistenceManager instance = new PersistenceManager();
-        instance.addNewNotifications(p);
+        instance.storePlayer(p);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
 
     /**
-     * Test of addNewMonsters method, of class PersistenceManager.
+     * Test of storeNotifications method, of class PersistenceManager.
      */
     @Test
-    public void testAddNewMonsters() {
-        System.out.println("addNewMonsters");
+    public void testStoreNotifications() {
+        System.out.println("storeNotifications");
         Player p = null;
         PersistenceManager instance = new PersistenceManager();
-        instance.addNewMonsters(p);
+        instance.storeNotifications(p);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of storeMonsters method, of class PersistenceManager.
+     */
+    @Test
+    public void testStoreMonsters() {
+        System.out.println("storeMonsters");
+        Player p = null;
+        PersistenceManager instance = new PersistenceManager();
+        instance.storeMonsters(p);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -111,7 +115,7 @@ public class PersistenceManagerTest {
     @Test
     public void testGetFriendList() {
         System.out.println("getFriendList");
-        int playerID = 0;
+        String playerID = "";
         PersistenceManager instance = new PersistenceManager();
         ArrayList expResult = null;
         ArrayList result = instance.getFriendList(playerID);
@@ -126,7 +130,7 @@ public class PersistenceManagerTest {
     @Test
     public void testGetNotificationList() {
         System.out.println("getNotificationList");
-        int playerID = 0;
+        String playerID = "";
         PersistenceManager instance = new PersistenceManager();
         ArrayList expResult = null;
         ArrayList result = instance.getNotificationList(playerID);
@@ -141,10 +145,207 @@ public class PersistenceManagerTest {
     @Test
     public void testGetMonsterList() {
         System.out.println("getMonsterList");
-        int playerID = 0;
+        String playerID = "";
         PersistenceManager instance = new PersistenceManager();
         ArrayList expResult = null;
         ArrayList result = instance.getMonsterList(playerID);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getPlayer method, of class PersistenceManager.
+     */
+    @Test
+    public void testGetPlayer() {
+        System.out.println("getPlayer");
+        String userID = "";
+        PersistenceManager instance = new PersistenceManager();
+        Player expResult = null;
+        Player result = instance.getPlayer(userID);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getPlayerIdAndServer method, of class PersistenceManager.
+     */
+    @Test
+    public void testGetPlayerIdAndServer() {
+        System.out.println("getPlayerIdAndServer");
+        String email = "";
+        PersistenceManager instance = new PersistenceManager();
+        String[] expResult = null;
+        String[] result = instance.getPlayerIdAndServer(email);
+        assertArrayEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isFriendRequestSent method, of class PersistenceManager.
+     */
+    @Test
+    public void testIsFriendRequestSent() {
+        System.out.println("isFriendRequestSent");
+        String playerOne = "";
+        String playerTwo = "";
+        PersistenceManager instance = new PersistenceManager();
+        boolean expResult = false;
+        boolean result = instance.isFriendRequestSent(playerOne, playerTwo);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of sendFriendRequest method, of class PersistenceManager.
+     */
+    @Test
+    public void testSendFriendRequest() {
+        System.out.println("sendFriendRequest");
+        String senderID = "";
+        String receiverID = "";
+        int receiverServerID = 0;
+        PersistenceManager instance = new PersistenceManager();
+        instance.sendFriendRequest(senderID, receiverID, receiverServerID);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of confirmFriendship method, of class PersistenceManager.
+     */
+    @Test
+    public void testConfirmFriendship() {
+        System.out.println("confirmFriendship");
+        String senderID = "";
+        int senderServer = 0;
+        String receiverID = "";
+        int receiverServer = 0;
+        PersistenceManager instance = new PersistenceManager();
+        instance.confirmFriendship(senderID, senderServer, receiverID, receiverServer);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of rejectFriendship method, of class PersistenceManager.
+     */
+    @Test
+    public void testRejectFriendship() {
+        System.out.println("rejectFriendship");
+        String senderID = "";
+        int senderServer = 0;
+        String receiverID = "";
+        int receiverServer = 0;
+        PersistenceManager instance = new PersistenceManager();
+        instance.rejectFriendship(senderID, senderServer, receiverID, receiverServer);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getHighscores method, of class PersistenceManager.
+     */
+    @Test
+    public void testGetHighscores() {
+        System.out.println("getHighscores");
+        String playerID = "";
+        PersistenceManager instance = new PersistenceManager();
+        ArrayList expResult = null;
+        ArrayList result = instance.getHighscores(playerID);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getPlayerUsername method, of class PersistenceManager.
+     */
+    @Test
+    public void testGetPlayerUsername() {
+        System.out.println("getPlayerUsername");
+        String playerID = "";
+        int serverID = 0;
+        PersistenceManager instance = new PersistenceManager();
+        String expResult = "";
+        String result = instance.getPlayerUsername(playerID, serverID);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getFriendRequestList method, of class PersistenceManager.
+     */
+    @Test
+    public void testGetFriendRequestList() {
+        System.out.println("getFriendRequestList");
+        String userID = "";
+        PersistenceManager instance = new PersistenceManager();
+        ArrayList expResult = null;
+        ArrayList result = instance.getFriendRequestList(userID);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of acceptFriendRequest method, of class PersistenceManager.
+     */
+    @Test
+    public void testAcceptFriendRequest() {
+        System.out.println("acceptFriendRequest");
+        String requestID = "";
+        String receiverID = "";
+        PersistenceManager instance = new PersistenceManager();
+        instance.acceptFriendRequest(requestID, receiverID);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of cancelFriendRequest method, of class PersistenceManager.
+     */
+    @Test
+    public void testCancelFriendRequest() {
+        System.out.println("cancelFriendRequest");
+        String requestID = "";
+        String receiverID = "";
+        PersistenceManager instance = new PersistenceManager();
+        instance.cancelFriendRequest(requestID, receiverID);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getMonstersForSale method, of class PersistenceManager.
+     */
+    @Test
+    public void testGetMonstersForSale() {
+        System.out.println("getMonstersForSale");
+        String playerID = "";
+        PersistenceManager instance = new PersistenceManager();
+        ArrayList expResult = null;
+        ArrayList result = instance.getMonstersForSale(playerID);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getMonstersForBreeding method, of class PersistenceManager.
+     */
+    @Test
+    public void testGetMonstersForBreeding() {
+        System.out.println("getMonstersForBreeding");
+        String playerID = "";
+        PersistenceManager instance = new PersistenceManager();
+        ArrayList expResult = null;
+        ArrayList result = instance.getMonstersForBreeding(playerID);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
@@ -181,78 +382,6 @@ public class PersistenceManagerTest {
     }
 
     /**
-     * Test of getPlayerID method, of class PersistenceManager.
-     */
-    @Test
-    public void testGetPlayerID() {
-        System.out.println("getPlayerID");
-        String email = "";
-        PersistenceManager instance = new PersistenceManager();
-        long expResult = 0L;
-        long result = instance.getPlayerID(email);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getFriendRequestList method, of class PersistenceManager.
-     */
-    @Test
-    public void testGetFriendRequestList() {
-        System.out.println("getFriendRequestList");
-        long id = 0L;
-        PersistenceManager instance = new PersistenceManager();
-        ArrayList expResult = null;
-        ArrayList result = instance.getFriendRequestList(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of sendFriendRequest method, of class PersistenceManager.
-     */
-    @Test
-    public void testSendFriendRequest() {
-        System.out.println("sendFriendRequest");
-        long fromID = 0L;
-        String emailTo = "";
-        PersistenceManager instance = new PersistenceManager();
-        instance.sendFriendRequest(fromID, emailTo);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of acceptFriendRequest method, of class PersistenceManager.
-     */
-    @Test
-    public void testAcceptFriendRequest() {
-        System.out.println("acceptFriendRequest");
-        long fromID = 0L;
-        String toEmail = "";
-        PersistenceManager instance = new PersistenceManager();
-        instance.acceptFriendRequest(fromID, toEmail);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of cancelFriendRequest method, of class PersistenceManager.
-     */
-    @Test
-    public void testCancelFriendRequest() {
-        System.out.println("cancelFriendRequest");
-        long fromID = 0L;
-        String toEmail = "";
-        PersistenceManager instance = new PersistenceManager();
-        instance.cancelFriendRequest(fromID, toEmail);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
      * Test of getErrorMessage method, of class PersistenceManager.
      */
     @Test
@@ -261,6 +390,21 @@ public class PersistenceManagerTest {
         PersistenceManager instance = new PersistenceManager();
         String expResult = "";
         String result = instance.getErrorMessage();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getMonster method, of class PersistenceManager.
+     */
+    @Test
+    public void testGetMonster() {
+        System.out.println("getMonster");
+        int monsterID = 0;
+        PersistenceManager instance = new PersistenceManager();
+        Monster expResult = null;
+        Monster result = instance.getMonster(monsterID);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
