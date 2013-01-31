1a340858e7b31432a91da815339181ed381befca
Llion Vaughan <lwv@aber.ac.uk>
2013-01-31 14:29:09 +0000

    New name Generator!

--- G12/src/MonsterMash/data/NameGenerator.java
+++ G12/src/MonsterMash/data/NameGenerator.java
@@ -2,60 +2,37 @@ package data;
 
 import java.util.*;
 
+/**
+ *
+ * @author Llionv
+ */
 public class NameGenerator {
 
-    private static List vocals = new ArrayList();
-    private static List startConsonants = new ArrayList();
-    private static List endConsonants = new ArrayList();
-    private static List nameInstructions = new ArrayList();
+    private static List startWords = new ArrayList();
+    private static List endWords = new ArrayList();
 
     public static String getName() {
-        String demoVocals[] = { "a", "e", "i", "o", "u", "ei", "ai", "ou", "j",
-        "ji", "y", "oi", "au", "oo" };
-        String demoStartConsonants[] = { "b", "c", "d", "f", "g", "h", "k",
-        "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z",
-        "ch", "bl", "br", "fl", "gl", "gr", "kl", "pr", "st", "sh",
-        "th" };
-        String demoEndConsonants[] = { "b", "d", "f", "g", "h", "k", "l", "m",
-        "n", "p", "r", "s", "t", "v", "w", "z", "ch", "gh", "nn", "st",
-        "sh", "th", "tt", "ss", "pf", "nt" };
-        String demoNameInstructions[] = { "vd", "cvdvd", "cvd", "vdvd" };
-        vocals.addAll(Arrays.asList(demoVocals));
-        startConsonants.addAll(Arrays.asList(demoStartConsonants));
-        endConsonants.addAll(Arrays.asList(demoEndConsonants));
-        nameInstructions.addAll(Arrays.asList(demoNameInstructions));
-        return firstCharUppercase(getNameByInstructions(getRandomElementFrom(nameInstructions)));
+        String thestartWords[] = {"Blessed", "Cursed", "Devious", "Forgotten", "Greater",
+            "Hidden", "Hood", "Lost", "Vile", "Blood", "Creeping",
+            "Sacred", "Whip", "Pixie", "Toad", "Dreadshredder", "Minature",
+            "Terrifying", "Primordial", "Freezing", "Mighty", "Rainbow",
+            "Slithering", "Babbling", "Withered"};
+        String theendWords[] = {"Snake", "Hawk", "Pudding", "Kraken", "Shark", "Hedgehog",
+            "Hacker", "Lurker", "Knight", "Slayer", "Demon", "Grotesque",
+            "Serpant", "Spectre", "Billy", "Lizard", "Swarm", "Strangler", "Mummy", ""};
+
+        startWords.addAll(Arrays.asList(thestartWords));
+        endWords.addAll(Arrays.asList(theendWords));
+
+        String name = startWords.get(randomInt(0, 25)) + " " + endWords.get(randomInt(0, 19));
+
+        return name;
     }
 
     private static int randomInt(int min, int max) {
         return (int) (min + (Math.random() * (max + 1 - min)));
     }
 
-    private static String getNameByInstructions(String nameInstructions) {
-        String name = "";
-        int l = nameInstructions.length();
-        for (int i = 0; i < l; i++) {
-            char x = nameInstructions.charAt(0);
-            switch (x) {
-                case 'v':
-                    name += getRandomElementFrom(vocals);
-                    break;
-                case 'c':
-                    name += getRandomElementFrom(startConsonants);
-                    break;
-                case 'd':
-                    name += getRandomElementFrom(endConsonants);
-                    break;
-            }
-            nameInstructions = nameInstructions.substring(1);
-        }
-        return name;
-    }
-    
-    private static String firstCharUppercase(String name){
-        return Character.toString(name.charAt(0)).toUpperCase() + name.substring(1);
-    }
-    
     private static String getRandomElementFrom(List<String> v) {
         return v.get(randomInt(0, v.size() - 1));
     }
